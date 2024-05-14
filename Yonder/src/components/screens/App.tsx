ce1d86bf67b72852075a9dd7eaf2efc99ab32c21
Andre Pham <andrepham33@gmail.com>
2024-05-14 14:42:33 +1000

    Home screen cleanup

--- Yonder/src/components/screens/App.tsx
+++ Yonder/src/components/screens/App.tsx
@@ -2,51 +2,25 @@ import VStack from "../containers/Stacks/VStack";
 import YonderText from "../base/YonderText";
 import YonderTypography from "../styling/YonderTypography";
 import YonderDimensions from "../styling/YonderDimensions";
-import YonderFlexImage from "../base/YonderFlexImage";
 import YonderCSS from "../styling/YonderCSS";
 import YonderImage, { YonderImageScale } from "../base/YonderImage";
-import { useEffect, useState } from "react";
-import Yonder from "../../language/Functions";
 
 function HomeScreen() {
-    const calculateTitleWidth = (): number => {
-        return Yonder.boundToRange(
-            window.innerWidth - 2 * YonderDimensions.screenPadding, 
-            YonderDimensions.minimumPageWidth - 2 * YonderDimensions.screenPadding, 
-            600
-        )
-    }
-
-    const [titleWidth, setTitleWidth] = useState(calculateTitleWidth());
-
-    useEffect(() => {
-        const handleResize = () => {
-            setTitleWidth(calculateTitleWidth())
-        };
-        window.addEventListener("orientationchange", handleResize);
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("orientationchange", handleResize);
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
     return (
         <div
             style={{
-                // padding: YonderDimensions.screenPadding,
+                padding: YonderDimensions.screenPadding,
                 minWidth: YonderDimensions.minimumPageWidth,
-                border: "1px solid red",
             }}
         >
-            <VStack style={{alignItems: "center",}}>
+            <VStack style={{ alignItems: "center", border: "1px solid red" }}>
                 <YonderImage
                     fileName="header.png"
-                    
                     scale={YonderImageScale.scaleToFill}
                     style={{
                         ...YonderCSS.noInterpolation,
-                        width: titleWidth,
+                        width: "100%",
+                        maxWidth: 600,
                         border: "1px solid blue",
                     }}
                 />
--- Yonder/src/language/Functions.ts
+++ Yonder/src/language/Functions.ts
@@ -1,9 +1,7 @@
 class Yonder {
-
     public static boundToRange(value: number, minValue: number, maxValue: number): number {
-        return Math.max(Math.min(maxValue, value), minValue)
+        return Math.max(Math.min(maxValue, value), minValue);
     }
-
 }
 
 export default Yonder;
